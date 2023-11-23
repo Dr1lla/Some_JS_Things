7a2b92765db8a4753085a1d54fe1103f1ac4bafb
Dr1lla <151372221+Dr1lla@users.noreply.github.com>
2023-11-23 20:09:49 +0200

    Create project with JS homeworks

---
+++ lesson_1/main.js
@@ -0,0 +1,45 @@
+let st1 = 'hello';
+let st2 = 'owu';
+let st3 = 'com';
+let st4 = 'ua';
+
+let nu1 = 1;
+let nu2 = 10;
+let nu3 = -999;
+let nu4 = 123;
+let nu5 = 3.14;
+let nu6 = 2.7;
+let nu7 = 16
+
+let bool1 = true;
+let bool2 = false;
+
+console.log(st1);
+console.log(st2);
+console.log(st3);
+console.log(st4);
+
+console.log(nu1);
+console.log(nu2);
+console.log(nu3);
+console.log(nu4);
+console.log(nu5);
+console.log(nu6);
+console.log(nu7);
+
+console.log(bool1);
+console.log(bool2);
+
+let firstName = "John ";
+let middleName = "Julius ";
+let lastName = "Rosenberg";
+
+let person = firstName + middleName + lastName;
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
