eeb32fee8ad397c59d9e984ba69c2d5eb7467e84
Dr1lla <151372221+Dr1lla@users.noreply.github.com>
2023-12-02 17:31:35 +0200

    lesson_3 is done.

--- lesson_3/main.js
+++ lesson_3/main.js
@@ -44,3 +44,40 @@ for (let i = 0; i < listOfItems.length; i++) {
 // Виведення закриваючого тегу </ul>
 document.write('</ul>');
 
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+// Користувачі зі статусом true
+console.log("Користувачі зі статусом true:");
+for (let i = 0; i < users.length; i++) {
+    if (users[i].status === true) {
+        console.log(users[i]);
+    }
+}
+
+// Користувачі зі статусом false
+console.log("Користувачі зі статусом false:");
+for (let i = 0; i < users.length; i++) {
+    if (users[i].status === false) {
+        console.log(users[i]);
+    }
+}
+
+// Користувачі, які старші за 30 років
+console.log("Користувачі, які старші за 30 років:");
+for (let i = 0; i < users.length; i++) {
+    if (users[i].age > 30) {
+        console.log(users[i]);
+    }
+}
